55f006013d680c62e9b7195fe33c66fed0515562
Thomas Green <tomgreen135@gmail.com>
2021-10-01 17:48:13 +0100

    Add form label component

---
+++ src/components/FormLabel.vue
@@ -0,0 +1,10 @@
+
+<template>
+    <label
+        class="block text-sm font-medium text-gray-700"
+        v-bind="$attrs"
+        :class="$attrs.for ? 'cursor-pointer' : null"
+    >
+        <slot />
+    </label>
+</template>
---
+++ src/components/TextArea.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="sm:col-span-6">
+        <label :for="name" class="block text-sm font-medium text-gray-700">
+            {{ label }}
+        </label>
+        <div class="mt-1">
+            <textarea
+                :id="name"
+                :name="name"
+                :required="required"
+                v-model="formData.description"
+                rows="3"
+                class="
+                    shadow-sm
+                    focus:ring-indigo-500
+                    focus:border-indigo-500
+                    block
+                    w-full
+                    sm:text-sm
+                    border border-gray-300
+                    rounded-md
+                "
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            required: true,
+        },
+        label: {
+            type: String,
+            required: true,
+        },
+    },
+};
+</script>
